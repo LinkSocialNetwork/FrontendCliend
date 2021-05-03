a9718a5445c35970b0183a8e0f34b9966b5ff68c
houstonlws <houstonhvrd@gmail.com>
2021-05-03 09:54:11 -0400

    notification tweaks

--- Angular/src/app/shared/components/notification/notification.component.ts
+++ Angular/src/app/shared/components/notification/notification.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Notifications } from '../../model/Notifications';
+import { Post } from '../../model/Post';
+import { User } from '../../model/User';
+import { GetUserService } from '../../services/get-user.service';
 
 @Component({
   selector: 'app-notification',
@@ -12,9 +15,16 @@ export class NotificationComponent implements OnInit {
   @Input()
   notification: Notifications;
 
-  constructor() { }
-
+  triggeredUser: User;
+  post: Post;
+  profilePic: string;
+  constructor(private getUserServ: GetUserService) { }
+  
   ngOnInit(): void {
+    this.getUserServ.getUserById(this.notification.triggeredId).subscribe(data => {
+      this.triggeredUser=data;
+      this.triggeredUser.profileImg;
+    })
   }
 
 }
