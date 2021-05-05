25a532c21d53b4ed5c60205356ce6be662965572
Kevin Desktop <kchilds2020@gmail.com>
2021-05-04 20:04:38 -0500

    cleaned up profile page

--- Angular/src/app/pages/profile/profile.component.ts
+++ Angular/src/app/pages/profile/profile.component.ts
@@ -25,7 +25,7 @@ export class ProfileComponent implements OnInit {
   appCom: HTMLElement;
   isChecked:boolean = false;
   
-  currentUser:User ={
+  currentUser:User = {
     userID: 0,
     userName: '',
     password: '',
@@ -40,24 +40,6 @@ export class ProfileComponent implements OnInit {
     following: []
   };
 
-  // We create another User to hold all changed fields
-  // We changed it from a FormGroup so it's easier to send as
-  // we're just sending the user object anyways
-  updatedUser:User = {
-    userID: 0,
-    userName: '',
-    password: '',
-    email: '',
-    dob: null,
-    profileImg: '',
-    bio: '',
-    posts: null,
-    likes: null,
-    firstName:'',
-    lastName:'',
-    following: []
-  }
-
   // This will hold the updated profile pic file
   updatedImage:File=null;
 
@@ -68,13 +50,8 @@ export class ProfileComponent implements OnInit {
 
   isOldPasswordValid = true;
   
-  constructor(private getUserService:GetUserService,
-    private userService:UserService,
-    private router:Router,
-    private getPostService:GetPostService,
-    private loginService:LoginService,
-    private imageServ:ImageUploadService,
-    private appComponent:AppComponent) {  }
+  constructor( private router:Router,
+    private loginService:LoginService) {  }
 
   ngOnInit(): void {
 
@@ -89,133 +66,10 @@ export class ProfileComponent implements OnInit {
           this.loginService.setCurrent(data);
           
           this.currentUser = data;
-          console.log("FROM PROFILE", this.currentUser)
 
-          // Initialize the updated user with the current fields
-          this.updatedUser = data;
         }
       }
     )
-  }
-
-  // Basically a ngModel for the updatedImage field
-  handleFileInput(files:FileList){
-    this.updatedImage = files.item(0);
-    console.log("file name:"+this.updatedImage.name);
-    console.log("file type:"+this.updatedImage.type);
-    console.log("file size:"+this.updatedImage.size);
-  }
-
-
-  checkIfPostIsLiked(post:Post):boolean{
-    let loggedInUser:User =this.currentUser;
-    for(var like of post.usersWhoLiked){//will search the post for the Like that connects the user and post
-
-      for(var likeOfUser of loggedInUser.likes){
-        if(like.likeId===likeOfUser.likeId){
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  // Updates the logged in user with field values from the form (if they are not empty)
-  // We set the updatedUser object to the currentUser object onInit, so if they are not changed, then they will all be the current values
-  // TODO: if the user inputs a value, but then deletes the value, the updateUser object field should be changed back to the currentUser object field
-  updateUserChecks(){
-    Swal.fire({
-      title: 'Updating',
-      allowEscapeKey: false,
-      allowOutsideClick: false,
-      showConfirmButton: false,
-      timer: 4000,
-      onOpen: () => {
-        Swal.showLoading();
-      }
-    });
-    
-    // let user:User;
-    // console.log("Checking if password checkmark is there");
-
-    // This checks if the user is trying to update their password via the toggle button
-    // If so, then we use the userService.checkOldPass with the entered in old password to verify if it's valid
-    // If not, we return and don't update anything
-    if(this.isChecked){
-      //check if new passwords are the same and not empty
-      if(this.newPassword1 != this.newPassword2 || this.newPassword1.length==0) {
-        Swal.fire({ icon: 'warning', title: "Passwords do not match", timer: 4000, showConfirmButton: true });
-      }
-      else {
-        // If the entered in current password value is not what's in the db,
-        // we don't update anything
-        /* this.updatedUser.password = this.oldPassword;
-        this.userService.checkOldPass(this.updatedUser).subscribe(data => {
-          this.isOldPasswordValid = data;
-          if(this.isOldPasswordValid){
-            this.updatedUser.password = this.newPassword1;
-            this.updateImageCheck();
-          
-          }else{
-            Swal.fire({ icon: 'warning', title: "Incorrect Old Password", timer: 4000, showConfirmButton: true });
-          }
-        }) */
-      }
-    }else{
-      //if password check box is unchecked, dont check anything above.
-      this.updateImageCheck();
-    }
-}
-
-  updateImageCheck(){
-// If there's a new image, upload it and set it to the updatedUser object
-      if(this.updatedImage != null){ 
-        // console.log('updatedImg: ', this.updatedImage); 
-        let file:FormData=new FormData;
-        file.append("file",this.updatedImage)
-        this.imageServ.imageUpload(file).subscribe(
-          data=>{
-            console.log("We got the url:"+data.message);
-            //Set it to the updated user
-            this.updatedUser.profileImg=data.message;
-            
-            console.log('updated user img url: ',this.updatedUser.profileImg);
-
-            // This will update the user by sending the updatedUser object through the service
-            this.updateUser();
-
-          }
-        );
-      }else{
-        this.updateUser();  
-      }
-  }
-
-  updateUser(){
-    this.userService.updateUser(this.updatedUser).subscribe(
-      response =>{
-        if(response === true)
-          Swal.fire({ icon: 'success', title: 'Done', timer: 4000, showConfirmButton: true });
-        else
-          Swal.fire({ icon: 'warning', title: 'Username or Email already taken', timer: 4000, showConfirmButton: true });
-        
-        this.ngOnInit();
-      }
-    );
-  }
-
-  filterPosts(event: any){
-    let matcher = new RegExp(event.target.value, "gi");
-
-    for (var i=0;i<document.getElementsByClassName("postHolder").length;i++) {
-      if (
-        matcher.test(document.getElementsByClassName("posttext")[i].innerHTML)
-      ) {
-        (<HTMLElement>document.getElementsByClassName("postHolder")[i]).style.display="";
-      } else {
-        (<HTMLElement>document.getElementsByClassName("postHolder")[i]).style.display="none";
-      }
-    }
   }
 
 }
